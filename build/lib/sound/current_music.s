e693f78c98a81ab9c8561f39757c42b4eb2f5856
= <wagner2004@gmx.de>
2025-06-14 01:29:47 +0200

    cleanup

--- build/lib/sound/current_music.s
+++
@@ -1,9 +0,0 @@
-;;; gcc for m6809 : Mar 17 2019 13:25:32
-;;; 4.3.6 (gcc6809)
-;;; ABI version 1
-;;; -mabi=bx -mint8 -fomit-frame-pointer -O2
-	.module	current_music.c
-	.globl	_current_music
-	.area	.data
-_current_music:
-	.word	_music_off
---
+++ setup_workspace.bat
@@ -0,0 +1,6 @@
+@REM the make process needs certain directories, without them it will not build
+@REM execute this script and build afterwards, everything should work... if not, you might need to add a certain folder structure (refer to make_c.bat)
+
+mkdir bin
+mkdir build
+mkdir build/rel
